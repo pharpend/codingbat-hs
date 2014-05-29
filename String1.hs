cacb2cf95ff6099a497245b311cbe75108430e3d
Peter Harpending <pharpend2@gmail.com>
2014-05-29 12:53:47 -0600

    Added comboString colution.

--- String1.hs
+++ String1.hs
@@ -62,3 +62,12 @@ firstTwo = take 2
 -- so "Hello" yields "ell". The string length will be at least 2.
 withoutEnd :: String -> String
 withoutEnd = init . tail
+
+-- |Given 2 strings, a and b, return a string of the form
+-- short+long+short, with the shorter string on the outside and the
+-- longer string on the inside. The strings will not be the same
+-- length, but they may be empty (length 0).
+comboString :: String -> String -> String 
+comboString x y
+  | length x <= length y  = x ++ y ++ x
+  | otherwise             = y ++ x ++ y

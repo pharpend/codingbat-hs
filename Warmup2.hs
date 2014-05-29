11d04937d268568acf11e088f46642bf924ac428
Peter Harpending <pharpend2@gmail.com>
2014-05-28 20:07:00 -0600

    Added more solutions.

--- Warmup2.hs
+++ Warmup2.hs
@@ -17,3 +17,16 @@ frontTimes s n = concat $ replicate n $ take 3 s
 -- starting with the first, so "Hello" yields "Hlo".
 stringBits :: String -> String
 stringBits s = map (s !!) $ filter even [0 .. (length s - 1)]
+
+-- |Given a non-empty string like "Code" return a string like
+-- "CCoCodCode".
+stringSplosion :: String -> String
+stringSplosion "" = ""
+stringSplosion s = (stringSplosion $ init s) ++ s
+
+-- |Given a string, return the count of the number of times that a
+-- substring length 2 appears in the string and also as the last 2
+-- chars of the string, so "hixxxhi" yields 1 (we won't count the end
+-- substring).
+last2 :: String -> Int
+last2 = undefined

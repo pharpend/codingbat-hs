1c2cf21b73e44fe9a0e20a49dd613cf7935decdc
Peter Harpending <pharpend2@gmail.com>
2014-05-29 13:20:40 -0600

    Added some solutions to Logic-1 problems.

---
+++ Logic1.hs
@@ -0,0 +1,63 @@
+-- |Logic1 - basic logic problems
+
+module Logic1 where
+
+-- |When squirrels get together for a party, they like to have
+-- cigars. A squirrel party is successful when the number of cigars is
+-- between 40 and 60, inclusive. Unless it is the weekend, in which
+-- case there is no upper bound on the number of cigars. Return true
+-- if the party with the given values is successful, or false
+-- otherwise.
+cigarParty :: Int -> Bool -> Bool
+cigarParty cigs wknd
+  | wknd      = 40 <= cigs
+  | otherwise = cigs `elem` [40..60]
+
+-- |You and your date are trying to get a table at a restaurant. The
+-- parameter "you" is the stylishness of your clothes, in the range
+-- 0..10, and "date" is the stylishness of your date's clothes. The
+-- result getting the table is encoded as an int value with 0=no,
+-- 1=maybe, 2=yes. If either of you is very stylish, 8 or more, then
+-- the result is 2 (yes). With the exception that if either of you has
+-- style of 2 or less, then the result is 0 (no). Otherwise the result
+-- is 1 (maybe).
+dateFashion :: Int -> Int -> Int
+dateFashion you date
+  | 2 >= you  = 0
+  | 2 >= date = 0
+  | 8 <= you  = 2
+  | 8 <= date = 2
+  | otherwise = 1
+
+-- |The squirrels in Palo Alto spend most of the day playing. In
+-- particular, they play if the temperature is between 60 and 90
+-- (inclusive). Unless it is summer, then the upper limit is 100
+-- instead of 90. Given an int temperature and a boolean isSummer,
+-- return true if the squirrels play and false otherwise.
+squirrelPlay :: Int -> Bool -> Bool
+squirrelPlay temp summer
+  | summer    = temp `elem` [60..100]
+  | otherwise = temp `elem` [60..90]
+
+-- |You are driving a little too fast, and a police officer stops
+-- you. Write code to compute the result, encoded as an int value:
+-- 0=no ticket, 1=small ticket, 2=big ticket. If speed is 60 or less,
+-- the result is 0. If speed is between 61 and 80 inclusive, the
+-- result is 1. If speed is 81 or more, the result is 2. Unless it is
+-- your birthday -- on that day, your speed can be 5 higher in all
+-- cases.
+caughtSpeeding :: Int -> Bool -> Int
+caughtSpeeding speed bday
+  | bday        = caughtSpeeding (speed - 5) False
+  | speed <= 60 = 0
+  | speed <= 80 = 1
+  | otherwise   = 2
+
+-- |Given 2 ints, a and b, return their sum. However, sums in the
+-- range 10..19 inclusive, are forbidden, so in that case just return
+-- 20.
+sortaSum :: Int -> Int -> Int
+sortaSum a b
+  | sum `elem` [10..19] = 20
+  | otherwise           = sum
+  where sum = a + b
